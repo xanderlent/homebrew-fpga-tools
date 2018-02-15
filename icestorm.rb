06ee01ee3bb975c0679b218c5b20047d1aba8629
Alexander Lent <git@xanderlent.com>
2018-02-15 00:56:04 -0800

    Icestorm is good now

--- icestorm.rb
+++ icestorm.rb
@@ -1,22 +1,18 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
- class Icestorm < Formula
-  desc "Project IceStorm - Lattice iCE40 FPGAs Bitstream Documentaion (Reverse Engineered)"
+class Icestorm < Formula
+  desc "Project IceStorm - Lattice iCE40 FPGA tools"
   homepage "http://www.clifford.at/icestorm/"
-  url "https://github.com/cliffordwolf/icestorm.git"
-  version "HEAD"
-  sha256 ""
+  head "https://github.com/cliffordwolf/icestorm.git"
   # TODO: What are the deps? Which are build-time?
-  depends_on "python3"# => :build
-  depends_on "libftdi0"
-  depends_on "pkg-config"
-   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
+  depends_on "python3" => :run # Most programs are py3 scripts?
+  depends_on "libftdi0" => :run # Used in iceprog?
+  depends_on "pkg-config" => :build # Speculative! TODO: Check if case!
+  def install
     # TODO: How to change prefix
-    system "make", "PREFIX=#{prefix}", "install" # if this fails, try separate make/make install steps
+    system "make", "PREFIX=#{prefix}", "install"
+    # TODO: Yeah yeah bad style low effort
+    # Don't understand why I need these, make install should do it but brew hates that...
   end
-   test do
+  test do
     # `test do` will create, run in and delete a temporary directory.
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
