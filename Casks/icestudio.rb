6d0bdb74ffe3b113116f75d9799f15bf600891d0
Alexander Lent <git@xanderlent.com>
2019-08-04 23:48:28 -0700

    Unfinished cask for icestudio

---
+++ Casks/icestudio.rb
@@ -0,0 +1,12 @@
+cask 'xanderlent/fpga-tools/icestudio' do
+  version '0.3.3'
+  sha256 'a6cef0ef192661a65e717d5670f8371b7da1021618d26267df501a7aa9ec3e94'
+
+  url "https://github.com/FPGAwars/icestudio/releases/download/v0.3.3/icestudio-0.3.3-osx64.dmg"
+  name 'icestudio'
+  homepage 'http://icestudio.readthedocs.io'
+
+  # TODO: UNFINISHED!
+
+  app ''
+end
